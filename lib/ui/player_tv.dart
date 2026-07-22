dcc24392f9ce1d3b1fdd5a296d5aecd2a44ff0ff
Mixypunk <mixypunk76@gmail.com>
2026-07-22 22:38:55 +0200

    Update Deezer and Fix bug add signature

--- lib/ui/player_tv.dart
+++ lib/ui/player_tv.dart
@@ -1,5 +1,5 @@
 import 'dart:ui';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide RepeatMode;
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import '../core/providers/player_provider.dart';
